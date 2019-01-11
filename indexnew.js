212b8a68c2a9cf8ed8913a9c3fc9794d7800cfd0
Valentina Adami <valeadami@gmail.com>
2019-01-11 19:17:37 +0100

    ritorna un oggetto json

--- indexnew.js
+++ indexnew.js
@@ -296,7 +296,9 @@ function callAVA(agent) {
             comandi=getComandi(c.output[0].commands);
            if (typeof comandi!=='undefined' && comandi.length>=1) {
               console.log('ho almeno un comando, quindi prosegui con l\' azione ' + comandi[0]);
-              agent.add(comandi.toString()); //
+              //agent.add(comandi.toString()); // ok, anche comandi[0] va bene
+              agent.add({ 'cmd': comandi[0], 
+              'strOutput':+strOutput});
            } else{
             agent.add('non ci sono comandi');
 
